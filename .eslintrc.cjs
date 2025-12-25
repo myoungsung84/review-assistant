4da4b60b0815fca601fc414679cdc5ea775d4bc6
myoungsung84 <myoungsung84@gmail.com>
2025-12-25 18:06:08 +0900

    chore: adjust eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,34 +15,25 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier', // 반드시 맨 마지막
+    'prettier',
   ],
 
   ignorePatterns: ['dist', 'dist-electron', '.eslintrc.cjs'],
 
   rules: {
-    // React Refresh (Vite + Electron)
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-
-    // Prettier를 ESLint 에러로
     'prettier/prettier': 'error',
-
-    // React 17+ JSX runtime
     'react/react-in-jsx-scope': 'off',
   },
 
   settings: {
-    react: {
-      version: 'detect',
-    },
+    react: { version: 'detect' },
   },
 
   overrides: [
     {
-      files: ['electron/**/*.ts'],
-      env: {
-        node: true,
-      },
+      files: ['electron/**/*.{ts,cts,mts}'],
+      env: { node: true },
     },
   ],
 }
